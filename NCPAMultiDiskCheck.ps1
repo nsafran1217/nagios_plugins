1858c6876320aecf002d3a1e9f3702313379dced
Safran <safranna2@upmc.edu>
2020-07-24 10:12:11 -0400

    added stuff

---
+++ NCPAMultiDiskCheck.ps1
@@ -0,0 +1,24 @@
+#Add all disk used together and report
+
+
+$disks = Get-WmiObject Win32_LogicalDisk | where drivetype -eq 3 | where deviceID -ne 'C:'
+
+
+
+$used = 0
+# Iterate through our disks array to get our free space and total size values in bytes for each disk
+# We append each array with the drive letter and its respective value
+foreach ($disk in $disks) {
+    # Eliminating DVD drives and anything else with no usage so we can avoid division-by-zero errors
+    if ($disk.Size -ne $disk.FreeSpace) {
+        $used =$used + ($disk.Size - $disk.FreeSpace)
+    }
+
+
+}
+
+$used = [math]::round($used/1GB)
+    $statusmessage = "OK $Used GB Used on all disks | DiskUsed=$used;;;"
+
+Write-Host $statusmessage
+#exit 0
---
+++ checkalldisks.ps1
@@ -0,0 +1,24 @@
+﻿#Add all disk used together and report
+
+
+$disks = Get-WmiObject Win32_LogicalDisk | where drivetype -eq 3 | where deviceID -ne 'C:'
+
+
+
+$used = 0
+# Iterate through our disks array to get our free space and total size values in bytes for each disk
+# We append each array with the drive letter and its respective value
+foreach ($disk in $disks) {
+    # Eliminating DVD drives and anything else with no usage so we can avoid division-by-zero errors
+    if ($disk.Size -ne $disk.FreeSpace) {
+        $used =$used + ($disk.Size - $disk.FreeSpace)
+    }
+
+
+}
+
+$used = [math]::round($used/1GB, 1)
+    $statusmessage = "OK - $Used GB Used on all disks | used=$used;;;"
+
+Write-Host $statusmessage
+#exit 0
